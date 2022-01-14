cd2a066b8cecf379e003fd0cde8b59000234eb8a
era0367 <eara0367@gmail.com>
2022-01-14 13:17:04 +0900

    Meta Generator
    - Meta Generator
    - KNU CSOS Activities of Daily Living(ADL) 1st Test Result

---
+++ Generator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Hl7.Fhir.Model;
+using ADL_FHIRGenerator;
+using static Common.Enums;
+
+namespace ADL_MetaGenerator
+{
+    public class Generator
+    {
+        public static readonly string institutionName = "Kyungpook National University Center of Self-Organizaing Software";
+        public static readonly string institutionIdentifier = "KNU CSOS";
+        public static readonly string authorName = "CSOS Tester";
+        public static readonly string title = "KNU CSOS Activities of Daily Living(ADL) 1st Test Result";
+        public static readonly string formatCode = "FHIR";
+        public static readonly string languageCode = "ko";
+        public static readonly string schemaId = "https://fhir.simplifier.net/KNU.MyHealthHub/StructureDefinition/MyObservation";
+        public static List<Metadata> GetMetadataList()
+        {
+            List<Metadata> metadataList = new();
+            List<Observation> observationList = ADL.GetAdlObservationList();
+            for(int i = 0; i < observationList.Count; i++)
+            {
+                Observation observation = observationList[i];
+                string observationJson = ADL.ToString(observation);
+                Metadata metadata = new Metadata();
+                metadata.author = Utility.GetAuthor(institutionIdentifier, institutionName, authorName, "");
+                metadata.availabilityStatus = AVAILABILITY_STATUS.AVAILABLE;
+                metadata.classCode = CLASS_CODE.PHYSICAL;
+                metadata.confidentialityCode = Utility.GetConfidentialityCode(observation.Subject.Identifier.Value);
+                metadata.creationTime = Utility.GetDateTime(DateTime.Now);
+                metadata.formatCode = formatCode;
+                metadata.healthcareFacilityType = HEALTHCARE_FACAILITY_TYPE_CODE.RESEARCH_INSTITUTION;
+                metadata.hash = Utility.GetHash(observationJson);
+                metadata.languageCode = languageCode;
+                metadata.patientId = observation.Subject.Identifier.Value;
+                metadata.size = Utility.GetSize(observationJson);
+                metadata.title = title;
+                metadata.schemaId = schemaId;
+                metadata.typeCode = Utility.GetTypeCode(observation.Code);
+                metadata.metadataId = Utility.GetMetdataId(metadata.patientId, metadata.author.person.identifier, metadata.creationTime.ToString());
+                metadataList.Add(metadata);
+            }
+            return metadataList;
+        }
+        
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,95 @@
+﻿using Common;
+using Hl7.Fhir.Model;
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using static Common.Enums;
+
+namespace ADL_MetaGenerator
+{
+    public class Utility
+    {
+        public static Author GetAuthor(string institutionName, string institutionIdentifier, string authorName, string authorIdentifier)
+        {
+            Author author = new Author();
+            author.institution = GetInstitution(institutionName, institutionIdentifier);
+            author.person = GetPerson(authorName, authorIdentifier);
+            return author;
+        }
+        private static Institution GetInstitution(string institutionName, string institutionIdentifier)
+        {
+            Institution institution = new Institution();
+            institution.identifier = ADL_FHIRGenerator.Utility.IdentifierManager.GetGuid(institutionIdentifier);
+            institution.name = institutionName;
+            return institution;
+        }
+        private static Common.Person GetPerson(string authorName, string authorIdentifier)
+        {
+            Common.Person person = new Common.Person();
+            if (0 >= authorIdentifier.Length)
+                authorIdentifier = authorName;
+            person.identifier = ADL_FHIRGenerator.Utility.IdentifierManager.GetGuid(authorIdentifier);
+            person.name = authorName;
+            person.type = TYPE_CODE.DATA_CONSUMER;
+            return person;
+        }
+        public static string GetConfidentialityCode(string identifier)
+        {
+            //@Todo
+            string confidentialityCode = TEST_GetConfidentialityCode();
+            return EncodeToBase64(confidentialityCode);
+        }
+        private static string EncodeToBase64(string str)
+        {
+            byte[] bytes = StringToBytes(str);
+            return Convert.ToBase64String(bytes);
+        }
+        private static byte[] StringToBytes(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+        private static string TEST_GetConfidentialityCode()
+        {
+            Target target = new Target();
+            target.no = false;
+            target.all = false;
+            target.family = true;
+            target.medicalInstitution = true;
+            target.publicInstitution = false;
+            target.researchInstitution = true;
+            target.insuranceCompany = false;
+            target.privateCompany = false;
+            return target.ToString();
+        }
+        public static string GetHash(string data)
+        {
+            SHA256Managed sha256Managed = new SHA256Managed();
+            byte[] bytes = StringToBytes(data);
+            byte[] encryptedBytes = sha256Managed.ComputeHash(bytes);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+        public static int GetSize(string data)
+        {
+            byte[] bytes = StringToBytes(data);
+            return bytes.Length;
+        }
+        public static Common.TypeCode GetTypeCode(CodeableConcept code)
+        {
+            Common.TypeCode typeCode = new Common.TypeCode();
+            typeCode.code = code.Coding[0].Code;
+            typeCode.system = code.Coding[0].System;
+            return typeCode;
+        }
+        public static string GetMetdataId(string patientId, string authorIdentifier, string creationTime)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            return String.Format("{0}|{1}|{2}", patientId, authorIdentifier, creationTime);
+        }
+        public static string GetDateTime(DateTime dateTime)
+        {
+            // return new FhirDateTime(new DateTimeOffset(dateTime)).ToString();
+            return new FhirDateTime(dateTime).ToString();
+        }
+    }
+}
